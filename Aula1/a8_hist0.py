9ffd10721f9c99a137fd3d71b341409506af0db7
Felipe Bombardelli <felipebombardelli@gmail.com>
2024-07-23 15:11:18 -0300

    corrigido um bug

--- Aula1/a8_hist0.py
+++ Aula1/a8_hist0.py
@@ -6,12 +6,12 @@ import cv2
 imagem = cv2.imread('imagens/messi5.jpg')
 
 # 2. Converte  a imagem de BGR para Escala de Cinza
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   #converte P&B
-cv2.imshow("Imagem P&B", img)
+imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)   #converte P&B
+cv2.imshow("Imagem P&B", imagem)
 cv2.waitKey(0)
 
 # 3. Calcula o Histograma da imagem
-h = cv2.calcHist([img], [0], None, [256], [0, 256])
+h = cv2.calcHist([imagem], [0], None, [256], [0, 256])
 
 # 4. Plota o histograma da imagem
 plt.figure()
@@ -20,7 +20,7 @@ plt.xlabel("Intensidade")
 plt.ylabel("Qtde de Pixels")
 plt.plot(h)
 plt.xlim([0, 256])
-# plt.hist(img.ravel(),256,[0,256])
+# plt.hist(imagem.ravel(),256,[0,256])
 plt.show()
 
 # 5. Mostra a imagem e espera uma tecla
